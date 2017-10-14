e6295393717ed54c0bdd7180deb4923d0c3ac1c0
Kravchuk <majerkravchuk@gmail.com>
2017-10-14 22:36:44 +0300

    Formatting items to select categories

--- app/admin/legal_library/document.rb
+++ app/admin/legal_library/document.rb
@@ -14,15 +14,39 @@ ActiveAdmin.register LegalLibrary::Document do
   end
 
   form do |f|
+    items = get_categories_for_select_input
+
     f.inputs do
       f.input :title
       f.input :category_id, label: 'Category',
                                    as: :select,
-                                   collection: LegalLibrary::Category.all { |r| [r.title, r.id] },
+                                   collection: get_categories_for_select_input,
                                    include_blank: false
       f.input :body
     end
 
     f.actions
   end
+end
+
+def get_categories_for_select_input
+  items = LegalLibrary::Category.all.map do |category|
+    {
+      title: category.title,
+      id: category.id,
+      parent: category.parent_category_id
+    }
+  end
+  items.select { |item| item[:parent].nil? }
+       .each_with_index { |item, i| item[:weight] = (i + 1) * 1000 }
+
+  items.select { |item| !item[:parent].nil? }
+       .each_with_index do |item, i|
+
+    parent = items.find { |parent| parent[:id] == item[:parent] }
+    item[:title] = "-  #{item[:title]}"
+    item[:weight] = parent[:weight] + 1
+  end
+  items.sort { |x, y| x[:weight] <=> y[:weight] }
+       .map { |item| [item[:title], item[:id]] }
 end
